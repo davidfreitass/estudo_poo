a97a4350bf88a40d32417477a12a162ad23fcca3
David Freitas <davidcnt@outlook.com>
2022-10-25 18:01:14 -0300

    Representacao em string da classe, representacao do objeto da classe
    Adicionado docstring de classe, docstring de metodo de classe e decorator para metodo estatico

--- vagas.py
+++ vagas.py
@@ -1,14 +1,23 @@
-"""
-Criar uma estrutura de classes pensando-se em um cenário de vagas de emprego
-"""
+from numpy.random import seed
+from numpy.random import shuffle
+
+seed(1)
 
 
 class Empresa:
+    """
+    Dados da empresa
+    """
+
     nome: str
     sede: str
 
 
 class Vaga(Empresa):
+    """
+    Dados da vaga
+    """
+
     tipo: str
     quant: int
 
@@ -18,8 +27,15 @@ class Vaga(Empresa):
         self.tipo = tipo
         self.quant = quant
 
+    def __repr__(self) -> str:
+        return f"Vaga(nome={self.nome}, sede={self.sede}, tipo={self.tipo}, quant={self.quant})"
+
 
 class Candidato:
+    """
+    Dados do candidato
+    """
+
     nome: str
     idade: int
     doc: str
@@ -31,7 +47,25 @@ class Candidato:
         self.doc = doc
 
     def my_vaga(self, vaga: Vaga):
+        """
+        Método para adicionar uma vaga ao candidato
+
+        :param Vaga vaga: Instância da classe vaga para ser atrelado ao candidato
+        """
         self.vaga = vaga
+
+    @staticmethod
+    def num_sorte() -> list:
+        sequence = [i for i in range(1, 60)]
+        shuffle(sequence)
+        return sequence
+
+    def __str__(self) -> str:
+        return (
+            f"Nome: {self.nome}, Idade: {self.idade}\n"
+            f"Vaga: {self.vaga.nome}, Sede: {self.vaga.sede}\n"
+            f"Tipo: {self.vaga.tipo}"
+        )
 
 
 vaga1 = Vaga("Distak", "Baraúnas", "Designer Gráfico", 1)
@@ -44,11 +78,11 @@ candidato1.my_vaga(cadastro1)
 candidato2.my_vaga(cadastro2)
 
 print("CANDIDATO 1: ")
-print(candidato1.nome, ",", candidato1.idade)
-print(candidato1.vaga.nome, "-", candidato1.vaga.sede)
-print(candidato1.vaga.tipo)
+print(candidato1)
 print("-" * 20)
 print("CANDIDATO 2: ")
-print(candidato2.nome, ",", candidato2.idade)
-print(candidato2.vaga.nome, "-", candidato2.vaga.sede)
-print(candidato2.vaga.tipo)
+print(candidato2)
+print("-" * 20)
+print(vaga1)
+print("-" * 20)
+print(candidato1.num_sorte())
